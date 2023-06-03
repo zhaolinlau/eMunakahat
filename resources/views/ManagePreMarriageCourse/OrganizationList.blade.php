46c9b1259e7deb234b34c567d9fa4446936503b5
Hidayati <myhidayatikasim@gmail.com>
2023-06-04 02:23:27 +0800

    b

--- resources/views/ManagePreMarriageCourse/OrganizationList.blade.php
+++ resources/views/ManagePreMarriageCourse/OrganizationList.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
 <nav class="navbar shadow-sm fixed-top" style="background-color: #2F9577">
     <div class="container-fluid">
         <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive"><i class="fa-solid fa-bars fa-xl"></i></button>
@@ -17,7 +18,6 @@
                 <li><a class="dropdown-item" href="">Logout</a></li>
             </ul>
         </div>
-        
     </div>
 </nav>
 <div class="container">
@@ -30,94 +30,103 @@
                 <div class="row ">
                     <div class="col">
                         <div class="border border-5 p-3 rounded-end rounded-start">
-                        <form class="row g-3">
-                            <div class="col-auto">
-                                <label for="organization" class="text-center" style="color: white">Pilih Anjuran</label>
-                                <input type="text" readonly class="form-control-plaintext" id="organization" value="">
-                            </div>
-                            <div class="col-auto">
-                                <label for="inputorganization" class="visually-hidden"></label>
-                                <input type="text" class="form-control" id="organization" placeholder="Organization">
-                            </div>
-                            <div class="col-auto">
-                                <button type="submit" class="btn btn-success mb-3">Cari</button>
-                            </div>
-                        </form>
-                            <table class="table table-bordered bg-light border-dark" style="color: black;">
-                                <thead class="table-dark border-light">
-                                    <tr >
-                                    <th scope="col">Bil</th>
-                                    <th scope="col">Anjuran</th>
-                                    <th scope="col">Tempat</th>
-                                    <th scope="col">Tarikh</th>
-                                    <th scope="col">Kapasiti Peserta</th>
-                                    <th scope="col">Kekosongan</th>
-                                    <th scope="col">Papar Lanjut</th>
-                                    <th scope="col">Daftar Penyertaan</th>
-                                    </tr>
+                            <h5 class="text-center" style="color: white">Carian Anjuran</h5>
+                            <div class="container">
+                              <table class="table" id="organizationTable">
+                                <thead>
+                                  <tr>
+                                    <th>Bil</th>
+                                    <th>Anjuran</th>
+                                    <th>Tempat</th>
+                                    <th>Tarikh</th>
+                                    <th>Kapasiti Peserta</th>
+                                    <th>Kekosongan</th>
+                                    <th>Papar Lanjut</th>
+                                    <th>Daftar Penyertaan</th>
+                                    </th>
+                                  </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
+                                  <tr>
                                     <td>1</td>
-                                    <td>Majlis Anugerah Kecemerlangan</td>
-                                    <td>Dewan Besar</td>
-                                    <td>20 Jun 2023</td>
+                                    <td>PEJABAT AGAMA ISLAM MARAN</td>
+                                    <td>DEWAN AUDITORIUM AL-GHAZALI PAID MARAN</td>
+                                    <td>28 - 29 JANUARI 2023</td>
                                     <td>100</td>
-                                    <td>50</td>
-                                    <td><a href="{{ route('user.viewOrganization') }}">Papar Lanjut</a></td>
-                                    <td><a href="{{ route('user.courseForm') }}">Daftar Penyertaan</a></td>
-                                    </tr>
+                                    <td>54</td>
+                                    <td><button class="btn"><i class="fa-solid fa-eye fa-xl"></i></button></td>
+                                    <td><button class="btn"><i class="fa-solid fa-pen-to-square fa-xl"></i></button></td>
+                                  <tr>
+                                    <td>2</td>
+                                    <td>PEJABAT AGAMA ISLAM TEMERLOH</td>
+                                    <td>DEWAN KULIAH MASHAS TEMERLOH</td>
+                                    <td>28 - 29 JANUARI 2023</td>
+                                    <td>84</td>
+                                    <td>-</td>
+                                    <td><button class="btn"><i class="fa-solid fa-eye fa-xl"></i></button></td>
+                                    <td><button class="btn"><i class="fa-solid fa-pen-to-square fa-xl"></i></button></td>
+                                  </tr>
+                                  <!-- Add more table rows here -->
                                 </tbody>
-                                </table>
-
-                                    <a class="btn btn-success " href="{{ route('user.viewOrganization') }}" role="button">Kembali</a>
-                                
-                        </div>
-                    </div> 
-                </div>
-            </div>                     
-        </div>     
-    </div>
-</div>
-
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasResponsiveLabel">
-            <span class="d-none d-lg-flex">Menu</span>
-            <span class="d-lg-none">e-Munakahat</span>
-        </h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
-    </div>
-    
-    <div class="offcanvas-body" >
-        <nav class="nav nav-pills flex-column" >
-            <form action="" class="d-lg-none" method="get" >
-                <div class="input-group" >
-                    <input class="form-control" type="text" placeholder="Search">
-                    <button class="btn btn-outline-info" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-            </form>
-            <br>
-            <a class="nav-link" href="">Profile</a>
-            <a class="nav-link"  href="{{ route('user.terms') }}">Kursus Pra Perkahwinan</a>
-            <a class="nav-link"  href="">Permohonan Berkahwin</a>
-            <a class="nav-link"  href="">Pendaftaran Perkahwinan</a>
-            <a class="nav-link"  href="">Khidmat Nasihat</a>
-        </nav>
-    </div>
-    
-    <div class="container">
-        <div class="row">
-            <div class="col text-center">
-                Copyright &COPY; <span id="year"></span> e-Munakahat. All Rights Reserved.
-            </div>
+                              </table>
+                              <div class="col-12">
+                                <button type="button" class="btn btn-success" href="{{ route('user.viewOrganization') }}" role="button">Kembali</button>
+                              </div>
+                            </div>
+                                <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+                                <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+                                <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+                                <script>
+                                    $(document).ready(function() {
+                                      $('#marriageTable').DataTable();
+                                    });
+                                  </script>
+                        </div> 
+                    </div>
+                </div>                     
+            </div>     
         </div>
     </div>
-    
-    <script>
-        const year = new Date().getFullYear()
-        document.getElementById('year').innerHTML = year;
-    </script>
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasResponsiveLabel">
+                <span class="d-none d-lg-flex">Menu</span>
+                <span class="d-lg-none">e-Munakahat</span>
+            </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
+        </div>
+        
+        <div class="offcanvas-body" >
+            <nav class="nav nav-pills flex-column" >
+                <form action="" class="d-lg-none" method="get" >
+                    <div class="input-group" >
+                        <input class="form-control" type="text" placeholder="Search">
+                        <button class="btn btn-outline-info" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                </form>
+                <br>
+                <a class="nav-link" href="">Profile</a>
+                <a class="nav-link"  href="{{ route('user.terms') }}">Kursus Pra Perkahwinan</a>
+                <a class="nav-link"  href="">Permohonan Berkahwin</a>
+                <a class="nav-link"  href="">Pendaftaran Perkahwinan</a>
+                <a class="nav-link"  href="">Khidmat Nasihat</a>
+            </nav>
+        </div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    Copyright &COPY; <span id="year"></span> e-Munakahat. All Rights Reserved.
+                </div>
+            </div>
+        </div>
+        
+        <script>
+            const year = new Date().getFullYear()
+            document.getElementById('year').innerHTML = year;
+        </script>
+    </div>
 </div>
 
 <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
