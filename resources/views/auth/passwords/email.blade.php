a9bbeb95ab1191536aac52069628f76148b3ffe2
Zhaolin Lau <zhaolinlau@gmail.com>
2023-06-11 11:01:37 +0800

    add bg color to email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
 	<div class="row justify-content-center h-100 align-items-center">
 		<div class="col-md-8 py-5">
 			<div class="card">
-				<div class="card-header">{{ __('Tetapan Semula Kata Laluan') }}</div>
+				<div class="card-header bg-primary">{{ __('Tetapan Semula Kata Laluan') }}</div>
 
 				<div class="card-body">
 					@if (session('status'))
