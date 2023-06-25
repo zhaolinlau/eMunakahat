47cfabc37d2f3a1efa62c9d7276da44a8a12f869
Zhaolin Lau <zhaolinlau@gmail.com>
2023-06-25 20:42:01 +0800

    fix syntax error for create marriage registration table

--- database/migrations/2023_06_11_225515_create_marriage_registration_table.php
+++ database/migrations/2023_06_11_225515_create_marriage_registration_table.php
@@ -16,11 +16,7 @@ return new class extends Migration
 			$table->string('Registration_Type');
 			$table->string('Registration_Date');
 			$table->string('Registration_Status');
-<<<<<<< HEAD:database/migrations/2023_06_08_194619_create__pendaftaran_perkahwinan_table.php
-			$table->foreignId('Applicant_ID')->references('Applicant_ID')->on('Applicant_Info');
-=======
 			$table->foreignId('Applicant_ID')->references('Applicant_ID')->on('applicant_info');
->>>>>>> cddf615ed2c4cc4b2044ac98d76800faa7e541a1:database/migrations/2023_06_11_225515_create_marriage_registration_table.php
 			$table->foreignId('User_ID')->references('id')->on('users');
 			$table->foreignId('Approval_ID')->references('Approval_ID')->on('marriage_approval');
 		});
